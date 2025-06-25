69146b0bc66bb02cc0f3fcd044f8f78a467f69a0
Salman <salmanalthof31@gmail.com>
2025-06-25 23:18:36 +0700

    chore: create project with jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+}
+
+export default createJestConfig(customJestConfig)
+
