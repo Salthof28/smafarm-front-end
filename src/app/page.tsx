9a7c8e075f92a677714822dc649a6385f2c62ea5
Salman <salmanalthof31@gmail.com>
2025-06-27 17:12:20 +0700

    chore: create service api and interface.ts file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className="w-full flex flex-col items-center">
       <Navbar/>
-      <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-[2rem] gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] mt-[3rem] w-full">
+      <main className="items-center justify-items-center min-h-screen p-[2rem] gap-[2rem] sm:p-20 font-[family-name:var(--font-geist-sans)] mt-[2rem] w-full">
+          <h2>test</h2>
           <h2>test</h2>
           <h2>test</h2>
           <h2>test</h2>
