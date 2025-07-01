a08ccc084f4c4d1d92289241f642e13c80b97ddd
Salman <salmanalthof31@gmail.com>
2025-07-02 00:25:35 +0700

    style: responsive for all device and change some color

--- src/app/animal/page.tsx
+++ src/app/animal/page.tsx
@@ -2,26 +2,31 @@ import AnimalCardList from "@/components/animal/animal-card";
 import Navbar from "@/components/navbar";
 import { mockAnimals, mockCategory } from "@/services/api";
 import { Animal, Category } from "@/types/interfaces";
+import { Square, SquareCheckBig } from "lucide-react";
 
 
 export default function AnimalPage() {
     const category: Category[] = mockCategory;
     const animals: Animal[] = mockAnimals;
     return(
-        <div className="w-full flex flex-col items-center min-h-screen overflow-x-hidden">
+        <div className="bg-amber-100 w-full flex flex-col items-center min-h-screen overflow-x-hidden">
             <Navbar/>
-            <main className="flex items-center flex-col mt-[12rem]" >
+            <main className="flex items-center flex-col mt-[6rem] lg:mt-[12rem]" >
                 {/* <h1 className="p-[8rem]">Hello Animal</h1> */}
-                <section className="flex flex-row px-[10vw] justify-center w-full">
-                    <section className="bg-white p-[2rem] shadow-lg/30 ring-[0.1rem] ring-black/5 min-w-[15rem] w-fit h-fit rounded-[0.5rem]">
+                <section className="flex flex-row px-[0.5rem] md:px-[1rem] 2xl:px-[10vw] justify-center w-full">
+                    <section className="hidden lg:block bg-amber-50 p-[2rem] shadow-lg/30 ring-[0.1rem] ring-black/5 min-w-[15rem] w-fit h-fit rounded-[0.5rem]">
                         <h4>Filter</h4>
                         <hr></hr>
-                        <p>Category</p>
+                        <p className="font-bold">Category</p>
                         {category.map((cat) => 
-                            <p key={cat.id}>{cat.name}</p>
+                            <div className="flex flex-row gap-[0.2rem] items-center">
+                                {/* <SquareCheckBig /> */}
+                                <Square />
+                                <p key={cat.id}>{cat.name}</p>
+                            </div>
                         )}
                     </section>
-                    <section className="flex flex-row gap-[2rem] flex-wrap w-fit justify-center">
+                    <section className="flex flex-row gap-[0.6rem] md:gap-[1rem] xl:gap-[2rem] flex-wrap w-fit justify-center">
                         {animals.map((animal) => (
                             <AnimalCardList key={animal.id} animal={animal} />
                         ))}
@@ -37,7 +42,6 @@ export default function AnimalPage() {
                         {animals.map((animal) => (
                             <AnimalCardList key={animal.id} animal={animal} />
                         ))}
-
                     </section>
                 </section>
             </main>
--- src/components/animal/animal-card.tsx
+++ src/components/animal/animal-card.tsx
@@ -15,18 +15,18 @@ export default function AnimalCardList ({ animal }: AnimalCardListProp) {
     }
 
     return (
-        <div className="flex flex-col bg-emerald-300 rounded-[1rem] w-[20rem]">
-            <img src={animal?.images?.[0]} className="object-cover w-full rounded-t-[1rem] h-[15rem]" alt={animal?.type} />
+        <div className="flex flex-col bg-amber-50 rounded-[1rem] w-[9rem] md:w-[14rem] xl:w-[20rem] shadow-lg/30 ring-[0.1rem] ring-black/5">
+            <img src={animal?.images?.[0]} className="object-cover w-full rounded-t-[1rem] h-[8rem] md:h-[10rem] xl:h-[15rem]" alt={animal?.type} />
             <div className="flex flex-col text-center px-[1rem]">
                 <h4 className="font-extralight">{animal?.type?.slice(0,14)}{animal?.type?.length > 15 ? '...' : ''}</h4>
-                <p className="pb-[1rem]">Category: {animal?.category?.name}</p>
-                <p className="text-justify">{animal?.description.slice(0,78)}{animal?.description?.length > 78 ? '...' : ''}</p>
+                <p className="pb-[0rem] md:pb-[1rem]">Category: {animal?.category?.name}</p>
+                <p className="text-justify hidden md:block">{animal?.description.slice(0,78)}{animal?.description?.length > 78 ? '...' : ''}</p>
             </div>
-            <div className="flex flex-col items-center gap-[1rem] px-[2rem] py-[1rem]">
-                <p>Rp {animal?.price}</p>
-                <div className="flex flex-row gap-[2rem] justify-between w-[100%]">
-                    <button onClick={() => handleRouter(animal?.id)} className="btn bg-emerald-400 shadow-lg/30">See Detail</button>
-                    <button className="btn bg-emerald-400 shadow-lg/30">Buy</button>
+            <div className="flex flex-col items-center gap-[1rem] px-[0.5rem] md:px-[1rem] xl:px-[2rem] py-[0.5rem] md:py-[1rem]">
+                <p className="font-bold">Rp {animal?.price}</p>
+                <div className="flex flex-row gap-[0.3rem] md:gap-[2rem] md:justify-between w-[100%]">
+                    <button onClick={() => handleRouter(animal?.id)} className="btn bg-emerald-500 hover:bg-emerald-700 hover:text-white xl:1rem">See Detail</button>
+                    <button className="btn bg-emerald-500 hover:bg-emerald-700 hover:text-white">Buy</button>
                 </div>
             </div>
         </div>
