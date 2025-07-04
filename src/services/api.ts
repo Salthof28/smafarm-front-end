e6a1d631be8e3090d64bdd3a61174b99699c8067
Salman <salmanalthof31@gmail.com>
2025-07-04 17:08:02 +0700

    refactor: add more image

--- src/services/api.ts
+++ src/services/api.ts
@@ -46,7 +46,7 @@ export let mockAnimals: Animal[] = [
         id: 1,
         type: 'Kuroge Washu',
         description: 'Elevate your wardrobe with this stylish black t-shirt featuring a striking monochrome mountain range graphic. Perfect for those who love the outdoors or want to add a touch of nature-inspired design to their look, this tee is crafted from soft, breathable fabric ensuring all-day comfort. Ideal for casual outings or as a unique gift, this t-shirt is a versatile addition to any collection.',
-        images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ5Re5iyDi9WAQfSAixdztS268YFnxg_r6ig&s", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRUzDTwjHwZmYevQetIygoP31rfU53WxAkmg&s", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWBJ9efNWjuhv2lJC75iiKkiqAXRDIcOHiRA&s"],
+        images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ5Re5iyDi9WAQfSAixdztS268YFnxg_r6ig&s", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRUzDTwjHwZmYevQetIygoP31rfU53WxAkmg&s", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWBJ9efNWjuhv2lJC75iiKkiqAXRDIcOHiRA&s", 'https://i.pinimg.com/736x/e2/be/03/e2be03862c0f05939418b4abf745b9f6.jpg', 'https://cdn.shopify.com/s/files/1/0711/8202/5015/files/03a4d4ec7d33d427c68504eaeb3fa73c_6430d3e7-eb6d-4962-a9d6-231446ff43c9.png?v=1738781836'],
         age: 2,
         price: 130000000,
         stock: 2,
