f64c5fb012fa9b74879e6aa9689dac3aa7553c84
Salman <salmanalthof31@gmail.com>
2025-08-18 17:06:26 +0700

    refactor: improve style

--- src/components/animal/animal-card.tsx
+++ src/components/animal/animal-card.tsx
@@ -29,26 +29,33 @@ export default function AnimalCardList ({ animals }: AnimalCardListProp) {
     }
 
     return (
-        <section className="flex flex-row gap-[0.6rem] md:gap-[1rem] xl:gap-[2rem] flex-wrap w-fit justify-center">
-        {currentAnimal.map((animal) => (
-            <div className="flex flex-col bg-amber-50 rounded-[1rem] w-[9rem] md:w-[14rem] xl:w-[20rem] shadow-lg/30 ring-[0.1rem] ring-black/5">
-                <img src={animal?.images?.[0]} className="object-cover w-full rounded-t-[1rem] h-[8rem] md:h-[10rem] xl:h-[15rem]" alt={animal?.type} />
-                <div className="flex flex-col text-center px-[1rem]">
-                    <h4 className="font-extralight">{animal?.type?.slice(0,14)}{animal?.type?.length > 15 ? '...' : ''}</h4>
-                    <p className="pb-[0rem] md:pb-[1rem]">Category: {animal?.category?.name}</p>
-                    <p className="text-justify hidden md:block">{animal?.description.slice(0,78)}{animal?.description?.length > 78 ? '...' : ''}</p>
-                </div>
-                <div className="flex flex-col items-center gap-[1rem] px-[0.5rem] md:px-[1rem] xl:px-[2rem] py-[0.5rem] md:py-[1rem]">
-                    <p className="font-bold">Rp {animal?.price}</p>
-                    <div className="flex flex-row gap-[0.3rem] md:gap-[2rem] md:justify-between w-[100%]">
-                        <button onClick={() => handleRouter(animal?.id)} className="btn bg-emerald-500 hover:bg-emerald-700 hover:text-white xl:1rem transition-opacity delay-200 active:scale-90">See Detail</button>
-                        <button className="btn bg-emerald-500 hover:bg-emerald-700 hover:text-white transition-opacity delay-200 active:scale-90">Buy</button>
+        <section className={`flex flex-row gap-[0.6rem] md:gap-[1rem] xl:gap-[2rem] flex-wrap w-full justify-center ${currentAnimal.length > 0 ? 'lg:justify-start' : 'justify-center min-h-[75vh]' } min-w-[71vw] xl:min-w-[77vw] 2xl:min-w-[70vw] items-center`}>
+        {currentAnimal.length > 0 ? (
+            currentAnimal.map((animal) => (
+                <div className="flex flex-col bg-amber-50 rounded-[1rem] w-[9rem] md:w-[14rem] xl:w-[20rem] shadow-lg/30 ring-[0.1rem] ring-black/5">
+                    <img src={animal?.images?.[0]} className="object-cover w-full rounded-t-[1rem] h-[8rem] md:h-[10rem] xl:h-[15rem]" alt={animal?.type} />
+                    <div className="flex flex-col text-center px-[1rem]">
+                        <h4 className="font-extralight">{animal?.type?.slice(0,14)}{animal?.type?.length > 15 ? '...' : ''}</h4>
+                        <p className="pb-[0rem] md:pb-[1rem]">Category: {animal?.category?.name}</p>
+                        <p className="text-justify hidden md:block">{animal?.description.slice(0,78)}{animal?.description?.length > 78 ? '...' : ''}</p>
+                    </div>
+                    <div className="flex flex-col items-center gap-[1rem] px-[0.5rem] md:px-[1rem] xl:px-[2rem] py-[0.5rem] md:py-[1rem]">
+                        <p className="font-bold">Rp {animal?.price}</p>
+                        <div className="flex flex-row gap-[0.3rem] md:gap-[2rem] md:justify-between w-[100%]">
+                            <button onClick={() => handleRouter(animal?.id)} className="btn bg-emerald-500 hover:bg-emerald-700 hover:text-white xl:1rem transition-opacity delay-200 active:scale-90">See Detail</button>
+                            <button className="btn bg-emerald-500 hover:bg-emerald-700 hover:text-white transition-opacity delay-200 active:scale-90">Buy</button>
+                        </div>
                     </div>
                 </div>
+            ))
+        ) : (
+            <div className="flex flex-col items-center">
+                <img src='/cow-not-found.png' className=" w-[10rem] h-[10rem] " />
+                <h2>Livestock Not Found</h2>
             </div>
-        ))}
+        )}
         {/* yg bawak mock semua */}
-        {currentAnimal.map((animal) => (
+        {/* {currentAnimal.map((animal) => (
             <div className="flex flex-col bg-amber-50 rounded-[1rem] w-[9rem] md:w-[14rem] xl:w-[20rem] shadow-lg/30 ring-[0.1rem] ring-black/5">
                 <img src={animal?.images?.[0]} className="object-cover w-full rounded-t-[1rem] h-[8rem] md:h-[10rem] xl:h-[15rem]" alt={animal?.type} />
                 <div className="flex flex-col text-center px-[1rem]">
@@ -115,8 +122,7 @@ export default function AnimalCardList ({ animals }: AnimalCardListProp) {
                     </div>
                 </div>
             </div>
-        ))}
-
+        ))} */}
         </section>
     );
 }
